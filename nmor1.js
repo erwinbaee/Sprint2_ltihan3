004ad4f8a515170760516a0837db938df30d2668
erwin sanjaya <erwin@sanajaya-hp.com>
2021-07-29 10:50:23 +0700

    jawaban

---
+++ nmor1.js
@@ -0,0 +1,104 @@
+var Laptop = {
+    Merek:'hp',
+    Ram:'4GB',
+    Warna:'Hitam',
+
+    keyboard:function (Mencetak_Huruf_Atau_Angka_Pada_Monitor){
+        return 'Mencetak_Huruf_Atau_Angka_Pada_Monitor'
+        
+    },
+    touchpad:function(Menggerakkan_Kursor){
+        return 'Menggerakkan_Kursor'
+    },
+}
+console.log(Laptop.merek);
+
+
+var BoxFile = { 
+    Merek:'Bindex',
+    Bahan:'Plastik',
+    Warna:'Abu-abu',
+
+    Wadah:function (Buku_Atau_File_Folder){
+        return 'Buku_Atau_File_Folder'
+        
+    },
+   
+}
+console.log(BoxFile);
+
+
+var Mouse = {
+    Merek:'Rexus',
+    Warna:'merah',
+    Made_in:'china',
+
+    pengganti_touchpad:function (Menggerakkan_Kursor){
+        return 'Menggerakkan_Kursor'
+
+    },
+
+
+}
+console.log(Mouse);
+
+
+var hp = {
+    Merek: 'Redmi note 10 Pro',
+    Warna: 'Hitam',
+    Ram: '8 GB',
+    Harga: '3.5 Juta',
+
+    Layar:function(Membuka_aplikasi){
+        return 'Membuka_aplikasi'
+
+    },
+    Tombol_yang_di_samping_layar:function(Menghidupkan_hp){
+        return 'Menghidupkan_hp'
+
+    },
+
+}
+console.log(hp);
+
+
+var Motor = {
+    Merek: 'Honda',
+    Tipe: 'Metic',
+    Harga: '35 Juta',
+
+    stang:function(Membelokkan_motor){
+        return 'Membelokkan_motor'
+    },
+    kunci:function(Menghidupkan_motor){
+        return 'Menghidupkan_motor'
+    },
+}
+console.log(Motor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nmor2.js
@@ -0,0 +1,12 @@
+var jualMobil ={
+    nama: `Tompel`,
+    merek:`Avanza`,
+    tahun: 2017,
+
+    iklan:function(){
+        document.write(`BU,saya ${this.nama},jualMobil ${this.merek},tahun ${this.tahun},bekas`)
+    }
+}
+jualMobil.iklan();
+
+
